78afe8b67a58cad90c14ac88817f6dcff08764b9
kristiansnts <epafroditus.kritian@gmail.com>
2023-09-01 15:58:36 +0700

    add styling with tailwind

--- tailwind-custom.js
+++ tailwind-custom.js
@@ -1,9 +1,16 @@
 tailwind.config = {
     theme: {
-      extend: {
-        colors: {
-          clifford: '#7A53F9',
+      extend: [
+        {
+            colors: {
+              clifford: '#7A53F9',
+            }
+        },
+        {
+            gridAutoRows: {
+                '300': '300px',
+              }
         }
-      }
+      ]
     }
   }
