15c588a138f9fc94e40837a7798a5ee38e68d326
kristiansnts <epafroditus.kritian@gmail.com>
2023-09-02 21:11:40 +0700

    add logic remove book and change iscomplete with ui

--- books.js
+++ books.js
@@ -1,5 +1,5 @@
 const openButton = document.getElementById('open');
-const dialog = document.getElementById('dialog');
+const modalAdd = document.getElementById('modal--add');
 const closeButton = document.getElementById('close');
 const overlay = document.getElementById('overlay');
 const submitButton = document.querySelector('button[type="submit"]');
@@ -10,7 +10,7 @@ const myLibrary = [];
 
 // show the overlay and the dialog
 openButton.addEventListener('click', function () {
-    dialog.classList.remove('hidden');
+    modalAdd.classList.remove('hidden');
     overlay.classList.remove('hidden');
 });
 
@@ -18,7 +18,7 @@ openButton.addEventListener('click', function () {
 closeButton.addEventListener('click', closeModal);
 
 function closeModal(){
-    dialog.classList.add('hidden');
+    modalAdd.classList.add('hidden');
     overlay.classList.add('hidden');
     inputList.forEach(input => input.value = '');
 }
@@ -60,22 +60,52 @@ submitButton.addEventListener('click', function(e){
     closeModal();
 })
 
+document.addEventListener('click', (e) => {
+    if(e.target.classList.contains('readChange')){
+        e.target.classList.toggle('bg-red-100');
+        e.target.classList.toggle('hover:bg-red-400');
+        const parentId = parseInt(e.target.parentNode.parentNode.parentNode.id);
+        switchRead(parentId);
+        (e.target.textContent == 'Not Read') ? e.target.textContent = 'Read' : e.target.textContent = 'Not Read';
+    }
+    if(e.target.classList.contains('removeCard')){
+        confirm('Are you sure remove this book?');
+        const parentId = parseInt(e.target.parentNode.parentNode.parentNode.id);
+        removeBookfromLibrary(parentId);
+    }
+})
+
+function switchRead(parentId){
+    myLibrary[parentId].isCompleted = !myLibrary[parentId].isCompleted;
+}
+
+function removeBookfromLibrary(parentId){
+    myLibrary.splice(myLibrary.indexOf(parentId, 1))
+    changeUI(myLibrary)
+}
 
 function changeUI(myLibrary){
     const contentBooks = document.querySelector('.books');
     const allBooks = document.querySelector('.all--book');
     let content = '';
     if(myLibrary.length == 0){
-        content = `<div class="notFound mt-20 row-start-2 col-start-2">
+        content = `<div class="notFound mx-auto mt-20 row-start-2 col-start-2 md:col-start-3 lg:col-start-4 xl:col-start-5">
         <img src="assets/notFound.png" class="">
-        <span class="block text-center mt-5 font-bold text-slate-500">Books is Empty!!</span>
+        <h1 class="text-center mt-5 font-bold text-slate-500">Books is Empty!!</h1>
       </div>`;
     }
 
     for(let i = 0; i < myLibrary.length; i++){
-        content += `<li class="relative">
-                        <img src="https://source.unsplash.com/random/200x300/?${myLibrary[i].title}">
-                        <span class="book--title absolute bottom-3 left-3 bg-sky-500 w-20 rounded text-center text-sm">${myLibrary[i].title}</span>
+        content += `<li class="w-full" id="${i}">
+                        <div class="card px-4 py-5 shadow-lg border-2 rounded text-center">
+                        <h1 class="font-bold text-xl">${myLibrary[i].title}</h1>
+                        <span class="block text-sm my-3">${myLibrary[i].author} (Author)</span>
+                        <p class="font-bold mb-4"${myLibrary[i].pages} Pages</p>
+                        <div class="card--cta">
+                            ${myLibrary[i].isCompleted ? '<a class="block readChange bg-green-100 bg-red-100 rounded my-2 py-2 cursor-pointer hover:bg-green-400 hover:bg-red-400">Read</a>' : '<a class="block readChange bg-red-100 bg-green-100 rounded my-2 py-2 cursor-pointer hover:bg-red-400 hover:bg-green-400">Not Read</a>'}    
+                            <a class="block removeCard bg-red-500 text-white rounded my-2 py-2 cursor-pointer">Remove</a>
+                        </div>
+                        </div>
                     </li>`;
         }
     
